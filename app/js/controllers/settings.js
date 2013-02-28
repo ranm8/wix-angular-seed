6e312b2cbfee74dc6bdb9e213aef92e5a31fe56a
ronena <ronena@codeoasis.com>
2013-02-28 18:58:40 +0200

    fixed last commit test

--- app/js/controllers/settings.js
+++ app/js/controllers/settings.js
@@ -15,14 +15,6 @@
          */
         $scope.user = user;
 
-        sdk.Settings.getSiteInfo(function(info) {
-            console.log(info);
-        });
-
-        sdk.Settings.getSiteInfo().then(function(info) {
-            console.log(info);
-        });
-
         /**
          * Watching the user model and making a call to our backend whenever it changes.
          */
