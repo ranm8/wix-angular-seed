efd5130d9d341d30cd037ee7050640bf12e8aa2c
ronena <ronena@codeoasis.com>
2013-03-05 18:09:22 +0200

    massive additions and modifications to the ui module

--- app/lib/ui-extension/ui.js
+++ app/lib/ui-extension/ui.js
@@ -87,13 +87,17 @@
          * @name Ui.uiSlider
          * @description
          * Makes a DOM div element to work as a slider element. It requires jQueryUI slider component to work but it
-         * will not throw any exceptions if it's not available.
+         * will not throw any exceptions if it's not available. See @link { http://jqueryui.com/slider } for more
+         * information.
          *
          * It can be configured with the following HTML attributes:
-         *   ui-range: whether the slider represents a range.
-         *   min: the minimum value of the slider.
-         *   max: the maximum value of the slider.
-         *   disabled: controls whether the slider should be disabled.
+         *   ui-min: The minimum value of the slider.
+         *   ui-max: The maximum value of the slider.
+         *   ui-orientation: Determines whether the slider handles move horizontally (min on left, max on right) or
+         *     vertically (min on bottom, max on top). Possible values: "horizontal", "vertical".
+         *   ui-step: Determines the size or amount of each interval or step the slider takes between the min and max.
+         *     The full specified value range of the slider (max - min) should be evenly divisible by the step.
+         *   disabled: Controls whether the element should be disabled.
          *
          * @example
          *   The following HTML tag would be turned into a slider element:
@@ -123,9 +127,10 @@
                     });
 
                     elm.slider({
-                        range: attr.uiRange,
-                        min: parseInt(attr.min),
-                        max: parseInt(attr.max)
+                        min: parseInt(attr.uiMin),
+                        max: parseInt(attr.uiMax),
+                        orientation: attr.uiOrientation,
+                        step: parseInt(attr.uiStep)
                     });
                 }
             };
@@ -135,12 +140,49 @@
          * @name Ui.uiDatePicker
          * @description
          * Makes a DOM input element with type text to work as a date picker element. It requires jQueryUI datepicker
-         * component to work but it will not throw any exceptions if it's not available.
+         * component to work but it will not throw any exceptions if it's not available. See @link
+         * { http://jqueryui.com/datepicker } for more information.
          *
          * It can be configured with the following HTML attributes:
-         *   ui-change-month: whether the month should be rendered as a dropdown instead of text.
-         *   ui-change-year: whether the year should be rendered as a dropdown instead of text. Use the ui-year-range
+         *   ui-alt-format: The dateFormat to be used for the altField option. This allows one date format to be shown
+         *     to the user for selection purposes, while a different format is actually sent behind the scenes. For a
+         *     full list of the possible formats see the formatDate function.
+         *   ui-auto-size: Set to true to automatically resize the input field to accommodate dates in the current
+         *     dateFormat.
+         *   ui-button-image: The URL for the popup button image. If set, the ui-button-text option becomes the alt value
+         *     and is not directly displayed.
+         *   ui-button-image-only: Whether the button image should be rendered by itself instead of inside a button
+         *     element.
+         *   ui-button-text: The text to display on the trigger button. Use in conjunction with the ui-show-on option
+         *     set to "button" or "both".
+         *   ui-change-month: Whether the month should be rendered as a dropdown instead of text.
+         *   ui-change-year: Whether the year should be rendered as a dropdown instead of text. Use the ui-year-range
          *     option to control which years are made available for selection.
+         *   ui-close-text: The text to display for the close link. Use the showButtonPanel option to display this
+         *     button.
+         *   ui-constrain-input: When true, entry in the input field is constrained to those characters allowed by the
+         *     current ui-date-format option.
+         *   ui-current-text: The text to display for the current day link. Use the ui-show-button-panel option to
+         *     display this button.
+         *   ui-date-format: The format for parsed and displayed dates.
+         *   ui-day-names: The list of long day names, starting from Sunday, for use as requested via the ui-date-format
+         *     option.
+         *   ui-day-names-min: The list of minimised day names, starting from Sunday, for use as column headers within
+         *     the datepicker.
+         *   ui-day-names-short: The list of abbreviated day names, starting from Sunday, for use as requested via the
+         *     dateFormat option.
+         *   ui-duration: Control the speed at which the datepicker appears, it may be a time in milliseconds or a
+         *     string representing one of the three predefined speeds ("slow", "normal", "fast").
+         *   ui-first-day: Set the first day of the week: Sunday is 0, Monday is 1, etc.
+         *   ui-goto-current: When true, the current day link moves to the currently selected date instead of today.
+         *   ui-hide-if-no-prev-next: Normally the previous and next links are disabled when not applicable
+         *     (see the minDate and maxDate options). You can hide them altogether by setting this attribute to true.
+         *   ui-is-rtl: Whether the current language is drawn from right to left.
+         *   ui-max-date: The maximum selectable date. When set to null, there is no maximum.
+         *   ui-min-date: The minimum selectable date. When set to null, there is no minimum.
+         *   ui-month-names: The list of full month names, for use as requested via the ui-date-format option.
+         *   ui-month-names-short: The list of abbreviated month names, as used in the month header on each datepicker
+         *     and as requested via the ui-date-format option.
          *   ui-year-range: The range of years displayed in the year drop-down: either relative to today's year
          *     ("-nn:+nn"), relative to the currently selected year ("c-nn:c+nn"), absolute ("nnnn:nnnn"), or
          *     combinations of these formats ("nnnn:-nn"). Note that this option only affects what appears in the
@@ -168,8 +210,29 @@
                     }
 
                     elm.datepicker({
+                        altFormat: attr.uiAltFormat,
+                        autoSize: toBool(attr.uiAutoSize),
+                        buttonImage: attr.uiButtonImage,
+                        buttonImageOnly: toBool(attr.uiButtonImageOnly),
+                        buttonText: attr.uiButtonText,
                         changeMonth: toBool(attr.uiChangeMonth),
                         changeYear: toBool(attr.uiChangeYear),
+                        closeText: attr.uiCloseText,
+                        constrainInput: toBool(attr.uiConstrainInput),
+                        currentText: attr.uiCurrentText,
+                        dateFormat: attr.uiDateFormat,
+                        dayNames: scope.$eval(attr.uiDayNames),
+                        dayNamesMin: scope.$eval(attr.uiDayNamesMin),
+                        dayNamesShort: scope.$eval(attr.uiDayNamesShort),
+                        duration: attr.uiDuration,
+                        firstDay: parseInt(attr.uiFirstDay),
+                        gotoCurrent: toBool(attr.uiGotoCurrent),
+                        hideIfNoPrevNext: toBool(attr.uiHideIfNoPrevNext),
+                        isRTL: toBool(attr.uiIsRtl),
+                        maxDate: attr.uiMaxDate,
+                        minDate: attr.uiMinDate,
+                        monthNames: scope.$eval(attr.uiMonthNames),
+                        monthNamesShort: scope.$eval(attr.uiMonthNamesShort),
                         yearRange: attr.uiYearRange,
                         onSelect: function(date) {
                             ctrl.$setViewValue(date);
@@ -182,8 +245,20 @@
         /**
          * @name Ui.uiDraggable
          * @description
-         * Activates a DOM element as a draggable element. It requires jQueryUI to be loaded into the page for
-         * it to have any effect.
+         * Makes a DOM element of any type to work as a draggable element. It requires jQueryUI draggable component to
+         * work but it will not throw any exceptions if it's not available. See @link { http://jqueryui.com/draggable }
+         * for more information.
+         *
+         * It can be configured with the following HTML attributes:
+         *   ui-axis: Constrains dragging to either the horizontal (x) or vertical (y) axis. Possible values: "x", "y".
+         *   ui-cursor: The CSS cursor during the drag operation.
+         *   ui-delay: Time in milliseconds after mousedown until dragging should start. This option can be used to
+         *     prevent unwanted drags when clicking on an element.
+         *   disabled: Controls whether the element should be disabled.
+         *
+         * @example
+         *   The following HTML tag would be turned into a draggable element:
+         *     <div ui-draggable ui-axis="x" ui-delay="100"></div>
          */
         .directive('uiDraggable', [function() {
             return {
@@ -192,8 +267,13 @@
                         return;
                     }
 
+                    scope.$watch(function() { return attr.disabled; }, function(value) {
+                        elm.draggable('option', 'disabled', value);
+                    });
+
                     elm.draggable({
                         axis: attr.uiAxis,
+                        cursor: attr.uiCursor,
                         delay: parseInt(attr.uiDelay)
                     });
                 }
@@ -203,8 +283,24 @@
         /**
          * @name Ui.uiAccordion
          * @description
-         * Activates a DOM element as an accordion element. It requires jQueryUI to be loaded into the page for
-         * it to have any effect.
+         * Makes a DOM div element to work as an accordion element. It requires jQueryUI accordion component to work
+         * but it will not throw any exceptions if it's not available. See @link { http://jqueryui.com/accordion } for
+         * more information.
+         *
+         * It can be configured with the following HTML attributes:
+         *   ui-active: Which panel is currently open.
+         *   ui-animate: If and how to animate changing panels.
+         *   ui-collapsible: Whether all the sections can be closed at once. Allows collapsing the active section.
+         *   ui-event: The event that accordion headers will react to in order to activate the associated panel.
+         *     Multiple events can be specificed, separated by a space.
+         *   ui-header: Selector for the header element, applied via .find() on the main accordion element. Content
+         *     panels must be the sibling immedately after their associated headers.
+         *   ui-height-style: Controls the height of the accordion and each panel.
+         *   disabled: Controls whether the element should be disabled.
+         *
+         * @example
+         *   The following HTML tag would be turned into a draggable element:
+         *     <div data-ui-accordion data-ui-height-style="auto" data-ui-header="div > h2">
          */
         .directive('uiAccordion', function() {
             return {
@@ -213,7 +309,15 @@
                         return;
                     }
 
+                    scope.$watch(function() { return attr.disabled; }, function(value) {
+                        elm.accordion('option', 'disabled', value);
+                    });
+
                     elm.accordion({
+                        active: attr.uiAction,
+                        animate: attr.uiAnimate,
+                        collapsible: attr.uicollapsible,
+                        event: attr.uiEvent,
                         header: attr.uiHeader,
                         heightStyle: attr.uiHeightStyle
                     });
